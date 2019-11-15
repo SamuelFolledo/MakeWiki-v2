7d61d8e1d26e4dae530399b71aeaba29abbf1406
Samuel Folledo <samuelfolledo@gmail.com>
2019-11-14 17:34:55 -0800

    [add] I barely understand what I did but the create now works. Warning, submit still does not save the new wiki page

--- wiki/forms.py
+++ wiki/forms.py
@@ -5,3 +5,11 @@ from wiki.models import Page
 class PageForm(forms.ModelForm):
     """ Render and process a form based on the Page model. """
     model = Page
+
+class CreatePageForm(forms.ModelForm):
+    # page_title = forms.CharField(label='Page-title', max_length=100)
+    # page_content = forms.CharField(label='Page-content', max_length=10000)
+
+    class Meta:
+        model = Page
+        fields = '__all__'
--- wiki/views.py
+++ wiki/views.py
@@ -1,10 +1,46 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views import View #for create page view
 
 from wiki.models import Page
+from wiki.forms import CreatePageForm
 
+class PageCreateView(View):
+    model = Page
+    def get(self, request):
+        # Code block for GET request
+        form = CreatePageForm()
+        return render(request, 'create.html', {'form': form})
+    def post(self, request):
+        # Code block for POST request
+        form = CreatePageForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
+        return render(request, 'create.html', {'form': form})
 
+# def new_page(request):
+#     # if this is a POST request we need to process the form data
+#     if request.method == 'POST':
+#         # create a form instance and populate it with data from the request:
+#         form = CreatePageForm(request.POST)
+#         # check whether it's valid:
+#         if form.is_valid():
+#             # process the data in form.cleaned_data as required
+#             # ...
+#             # redirect to a new URL:
+#             return HttpResponseRedirect('/thanks/')
+
+#     # if a GET (or any other method) we'll create a blank form
+#     else:
+#         form = CreatePageForm()
+
+#     return render(request, 'create.html', {'form': form})
+    
 
 class PageListView(ListView):
     """ Renders a list of all Pages. """
